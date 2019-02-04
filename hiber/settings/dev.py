b6f8cef315c09952c332082224fdc96d033e0161
Mark Cabanero <me@markcabanero.com>
2019-02-04 16:15:02 -0500

    Initalize framework (#2)
    Start our journey on developing!

---
+++ hiber/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '=cq64)k5m39kcf0lq@q_^$s1xj@hasgh!fgoe4g_y@8&0y2hck'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass

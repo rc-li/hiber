a0963b42a3856c7a0e499bcafbc43cc55bfe8efc
Mark Cabanero <me@markcabanero.com>
2019-02-26 16:05:42 -0500

    Clarify certain fields with their human readable equivalent
    - Used Wagtail's ImageRenditionField to at least get some media
    equivalent
    - Used Django's built-in choices translation to get rarity to show
    properly
    - Used Django Extra Fields to get size() and pups() to be serialized
    properly over API

--- hiber/apps/api/serializers.py
+++ hiber/apps/api/serializers.py
@@ -1,13 +1,23 @@
-from rest_framework import serializers
 from django.contrib.auth.models import User
+from drf_extra_fields.fields import FloatRangeField, IntegerRangeField
+from rest_framework import serializers
+from wagtail.images.api.fields import ImageRenditionField
 from ..bathouse.models import Bat
 
 
 class BatSerializer(serializers.HyperlinkedModelSerializer):
+    # TODO: Get base image exposed over API as well
+    #       Get current image to display an absolute path over API
+    #       Properly serialize other fields (habits, risk, risk_scope)
+    bat_image = ImageRenditionField('fill-200x200')
+    rarity = serializers.CharField(source='get_rarity_display')
+    size = FloatRangeField()
+    pups = IntegerRangeField()
+
     class Meta:
         model = Bat
         fields = ('id', 'common_name', 'scientific_name', 'rarity', 'habits',
-                  'size', 'pups', 'risk', 'risk_scope')
+                  'size', 'pups', 'risk', 'risk_scope', 'bat_image')
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
